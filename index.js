a18b023cb366659ec9e17fc591e3782247f8ceb6
Himanshu-Sharma713 <himanshusb713@gmail.com>
2025-03-08 20:22:23 +0530

    setup project

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+// import files / folder
 import express from "express";
+import path from "node:path";
 
+// create metods
 const app = express();
 const PORT = 3210;
 
+// set methods
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(import.meta.dirname, "public")));
+
+// route
 app.get("/", (req, res) => {
-  res.send("Welcome");
+  res.render("index");
 });
 
+// create server rendering
 app.listen(PORT, () => {
   console.log(`Server Running at http://localhost:${PORT}/`);
 });
