b859a5b409ba878432380042697bd9cc3742b9a1
Himanshu-Sharma713 <himanshusb713@gmail.com>
2025-03-10 00:10:14 +0530

    ok for it

--- index.js
+++ index.js
@@ -20,18 +20,13 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/files/:filename", (req, res) => {
-  fs.readFile(`/files/${req.params.filename}`, "utf-8", (err, fileData) => {
-    if (err) console.error("Error: ", err);
-    else res.render('show');
-  });
-});
-
 app.post("/create", (req, res) => {
   fs.writeFile(
     `./files/${req.body.title.split(" ").join("")}.txt`,
     `${req.body.details}`,
-    res.redirect("/")
+    () => {
+      res.redirect("/");
+    }
   );
 });
 
